fab8f94e5fb2a20e870d91b537f290e599a6e7bb
Vinicius <vtm@cin.ufpe.br>
2021-07-23 16:58:16 -0300

    Corrigindo AlusrcB

--- parts_made/muxs/mux_ALUsrcB.v
+++ parts_made/muxs/mux_ALUsrcB.v
@@ -8,8 +8,8 @@ module mux_ALUsrcB(
     wire [31:0] A1;
     wire [31:0] A2;
 
-    assign A1 = (selector) ? 32'b00000000000000000000000000000100 : data_memB;
-    assign A2 = (selector) ? data_sgnExtnd : A1;
-    assign src_B = (selector[1]) ? data_shiftL2 : A2 ;
+    assign A1 = (selector[0]) ? 32'b00000000000000000000000000000100 : data_memB;
+    assign A2 = (selector[0]) ? data_sgnExtnd : data_shiftL2;
+    assign src_B = (selector[1]) ?  A2 : A1 ;
 
 endmodule
