a3c0e8c9cd9401e2f89da74670872d884dd65f4a
Vinicius <vtm@cin.ufpe.br>
2021-07-23 21:01:45 -0300

    Adicionando Sign Extends

---
+++ parts_made/sign_extends/sign_extend8to32.v
@@ -0,0 +1,8 @@
+module sign_extend8to32(
+    input wire [7:0] data_int,
+    output wire [31:0] data_out
+);
+
+    assign data_out = (data_in[15]) ? {{24{1'b1}}, data_in} : {{24{1'b0}}, data_in}
+
+endmodule
