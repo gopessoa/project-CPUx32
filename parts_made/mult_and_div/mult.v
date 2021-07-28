89323a22636cda5b3128e7761ab0a6c1e6ba87b0
gopessoa <gop2@cin.ufpe.br>
2021-07-27 22:36:59 -0300

    mult atualizado

--- parts_made/mult_and_div/mult.v
+++ parts_made/mult_and_div/mult.v
@@ -6,12 +6,13 @@ module mult (
 
     output wire [31:0] hi,
     output wire [31:0] low,
+    output busy;
 
 );
     // m = multiplicand, q = multiplier, r = result
     // c = count
     reg [31:0] m, q, r; 
-    reg [3:0] c;
+    reg [31:0] c;
     reg test;
     
     wire [31:0] sum, diff;
@@ -22,32 +23,33 @@ module mult (
         q <= multiplier;
         r <= 32'b0;
         test <= 1'b0;
-        c <= 4'b0;
+        c <= 32'b0;
       end 
       else begin
         case ({q[0], test})
         2'b0_1 : {r, q, test} <= {sum[31], sum, q};
         2'b1_0 : {r, q, test} <= {diff[31], diff, q};
-        default: {r, q, test} <= {r[31, r, q]};
+        default: {r, q, test} <= {r[31], r, q};
         endcase
         c <= c + 1'b1;
       end
     end
 
     alu add (r, m, 1'b0, sum);
-    alu sub (r, m, 1'b1, diff);
+    alu sub (r, ~m, 1'b1, diff);
 
-    assign hi = {r, q};
-    assign low = {c < 8};
+    assign hi = {r, q}[31:0];
+    assign low = {r, q}[64:32]
+    assign busy = {c < 32};
 
 endmodule
 
 //alu para as operações de soma e subtração
 //cin = carry-in
 module alu(
-    input cin;
     input [31:0] a;
     input [31:0] b;
+    input cin;
     output [31:0] out;
 );
     assign out = a + b + cin;
