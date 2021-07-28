de397eed82edbfe72ada09d54e1093246bec9db7
gopessoa <gop2@cin.ufpe.br>
2021-07-28 14:30:21 -0300

    mult final

--- parts_made/mult_and_div/mult.v
+++ parts_made/mult_and_div/mult.v
@@ -1,11 +1,12 @@
 module mult (
     input [31:0] a,
     input [31:0] b,
+    input clock;
     output [31:0] hi,
     output [31:0] low
 );
-    parameter bits = 32,
-    parameter counter = 'bits/2;
+    parameter bits = 32;
+    parameter counter = bits/2;
     reg [2:0] test_case[counter-1:0];
     reg [32:0] collection_1[counter-1:0];
     reg [63:0] accumulator[counter-1:0];
@@ -16,10 +17,10 @@ module mult (
 
     assign inv_a = {~a[31], ~a}+1;
 
-    always @(a or b or inv_a) begin
+    always @(clock) begin
       test_case[0] = {b[1], b[0], 1'b0};
       for(i=1; i<counter; i=i+1)
-        test_case[i] = {b[2*i+1], b[2*i], b[2*i-1]};
+      test_case[i] = {b[2*i+1], b[2*i], b[2*i-1]};
       for(i=0; i<counter; i=i+1)
         begin
           case(test_case[i])
@@ -32,11 +33,11 @@ module mult (
 
           accumulator[i] = $signed (collection_1[i]);
           for(j=0; j<i; j=j+1)
-            accumulator[i] = {accumulator[i], 2'b00};
+          accumulator[i] = {accumulator[i], 2'b00};
         end
         product = accumulator[0];
-        for (i=0; i<counter; i=i+1)
-          product = product + accumulator[i];
+        for (i=1; i<counter; i=i+1)
+        product = product + accumulator[i];
     end
 
     assign hi = product[31:0];
