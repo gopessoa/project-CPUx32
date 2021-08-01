b0c89808dd8b7e7b022e1cb8bd7a42edbf931758
Betmann <pedro@meira.com>
2021-08-01 05:47:59 -0300

    loadbmem ajeitado

--- parts_made/UC.v
+++ parts_made/UC.v
@@ -431,7 +431,7 @@ reg [2:0]   CONTADOR;
  
             RegWrite = 1'b0;
             LoadAMem = 1'b0;
-            LoadBMem = 2'b00;
+            LoadBMem = 1'b00;
 
             //NÃO USADOS
             MemDataRegLoad = 1'b0;
